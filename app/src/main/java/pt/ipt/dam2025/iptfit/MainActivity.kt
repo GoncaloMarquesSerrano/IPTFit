8436e052655e55f1295b96f43694489ddf2699c2
Diogo Jorge <diogovj2262005@gmail.com>
2025-12-05 17:35:28 +0000

    visual

--- app/src/main/java/pt/ipt/dam2025/iptfit/MainActivity.kt
+++ app/src/main/java/pt/ipt/dam2025/iptfit/MainActivity.kt
@@ -1,20 +1,35 @@
 package pt.ipt.dam2025.iptfit
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // set initial fragment
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())
         }
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_activity -> replaceFragment(ActivityFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
